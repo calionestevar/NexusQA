0c52b77e18e299ec33c441f3ecf1df2d6e6ec808
Sam Patchet <calionestevar88@gmail.com>
2026-01-09 21:53:57 -0600

    Add AddressInfoTypes.h include to PalantirRequest.test.cpp

--- Source/Nexus/Palantir/Private/Tests/PalantirRequest.test.cpp
+++ Source/Nexus/Palantir/Private/Tests/PalantirRequest.test.cpp
@@ -4,6 +4,7 @@
 #include "Sockets.h"
 #include "SocketSubsystem.h"
 #include "IpAddress.h"
+#include "AddressInfoTypes.h"
 
 /**
  * Sample tests demonstrating network request tracing with PalantírRequest.
