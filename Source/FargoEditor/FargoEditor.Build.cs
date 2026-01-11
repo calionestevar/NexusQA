1f7acecbf8a66908efd74e3bb1d816ac0b4655b6
Sam Patchet <calionestevar88@gmail.com>
2026-01-10 21:12:37 -0600

    Add FargoEditor module for holding all Editor-related functions

---
+++ Source/FargoEditor/FargoEditor.Build.cs
@@ -0,0 +1,25 @@
+using UnrealBuildTool;
+
+public class FargoEditor : ModuleRules
+{
+    public FargoEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        Type = ModuleType.Editor;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "UnrealEd",   // GEditor, PIE, etc.
+            "Nexus"       // Core runtime module
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "Slate",
+            "SlateCore",
+            "EditorSubsystem"
+        });
+    }
+}
